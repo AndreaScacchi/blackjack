0ca9ff72b886de09e2012e578aaa523b26df5ea3
AndreaScacchi <andreascacchi10@gmail.com>
2022-08-17 11:07:28 +0200

    Card class declared

--- script.py
+++ script.py
@@ -1,1 +1,8 @@
 import random
+
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+    def __str__(self):
+        return f"{self.rank['rank']} of {self.suit}"
